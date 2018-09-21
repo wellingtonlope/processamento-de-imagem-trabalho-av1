914a2b13cb4dce28134cba21f740c947307a2e55
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-09-20 21:03:02 -0400

    Ajuste solução da questão 10 ✌️

--- question_10.py
+++ question_10.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from skimage import io
 
 
-def imEqHist(img, rows_i, rows_f, cols_i, cols_f):
+def imEqHist(img, img_out, rows_i, rows_f, cols_i, cols_f):
 	"""
 	realiza equalizacao de histograma
 	em uma imagem (8 bits) em escala de cinza
@@ -31,21 +31,9 @@ def imEqHist(img, rows_i, rows_f, cols_i, cols_f):
 		im_hn_ac[k] = im_hn_ac[k - 1] + im_hn_ac[k]
 		s_k[k] = round(255 * im_hn_ac[k])
 
-	# for i in range(rows_i, rows_f):
-	# 	for j in range(cols_i, cols_f):
-	# 		img[i, j] = s_k[img[i, j]]
-	img[rows_i + 1, cols_i + 1] = s_k[img[rows_i + 1, cols_i + 1]]
+	img_out[rows_i + 1, cols_i + 1] = s_k[img[rows_i + 1, cols_i + 1]]
 
-	im_hEq = [0] * 256
-	im_hnEq = im_hEq.copy()
-
-	for i in range(rows_i, rows_f):
-		for j in range(cols_i, cols_f):
-			im_hEq[img[i, j]] += 1
-			im_hnEq[img[i, j]] += 1 / MN
-
-	# return [im_h, im_hn, im_hEq, im_hnEq, img]
-	return img
+	return img_out
 
 
 path = 'img/'
@@ -59,7 +47,7 @@ img_out = img.copy()
 
 for i in range(1, rows - 1):
 	for j in range(1, cols - 1):
-		imEqHist(img_out, i - 1, i + 1, j - 1, j + 1)
+		imEqHist(img, img_out, i - 1, i + 1, j - 1, j + 1)
 
 plt.figure('In')
 io.imshow(img)
